66325c12d92eb3bc87049edb172d3586b9af2c00
Yassir <james.iharo@gmail.com>
2023-09-20 18:47:57 +0100

    env

--- sente.c
+++ sente.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * my_setenv this function responsible for
+ * the env value
+ *
+ * Return : 0 or NULL return failed.
+ */
+
 int my_setenv(const char *name, const char *value, int overwrite) {
 
 	char *existing_value = getenv(name);
 
 	if (existing_value != NULL && !overwrite) {
 
-		return 0;
+		return (0);
 	} else {
 
 		char *env_var = (char *)malloc(strlen(name) + strlen(value) + 2); 
 		if (env_var == NULL) {
 
-			return -1;
+			return (-1);
 		}
 
 
@@ -22,9 +29,9 @@ int my_setenv(const char *name, const char *value, int overwrite) {
 		if (putenv(env_var) != 0) {
 
 			free(env_var);
-			return -1;
+			return (-1);
 		}
 
-		return 0;
+		return (0);
 	}
 }
