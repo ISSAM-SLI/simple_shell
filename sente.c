6f1eb26378bf5239f0c8df4b75ad0c4162e68f58
ISSAM <issam.slimani.maroc@gmail.com>
2023-09-20 19:14:00 +0100

    another

--- sente.c
+++ sente.c
@@ -15,22 +15,8 @@ int my_setenv(const char *name, const char *value, int overwrite) {
 	if (existing_value != NULL && !overwrite) {
 
 		return (0);
-	} else {
-
-		char *env_var = (char *)malloc(strlen(name) + strlen(value) + 2); 
-		if (env_var == NULL) {
-
-			return (-1);
-		}
-
-
-
-		if (putenv(env_var) != 0) {
-
-			free(env_var);
-			return (-1);
-		}
-
-		return (0);
+	}
+	int add(int a, int b) {
+		    return a + b;
 	}
 }
