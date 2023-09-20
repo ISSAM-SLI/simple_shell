8532adfc2cd506ee889baad0cf8d10d5c9c9bab7
ISSAM <issam.slimani.maroc@gmail.com>
2023-09-20 18:50:03 +0100

    jhkehj

---
+++ customized_strtok.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+static char *strtok_custom(char *str, const char *delim) 
+{
+	static char *next_token = NULL;
+	if (str != NULL) 
+	{
+		next_token = str;
+	} 
+	else if (next_token == NULL) 
+	{
+		return NULL;
+	}
+
+	char *token_start = next_token;
+	char *token_end = strpbrk(next_token, delim);
+
+	if (token_end != NULL) 
+	{
+		*token_end = '\0';
+		next_token = token_end + 1;
+	} else 
+	{
+		next_token = NULL;
+	}
+
+	return token_start;
+}
